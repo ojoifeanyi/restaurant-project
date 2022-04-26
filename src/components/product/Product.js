b5fcddedd1bcc6ca24f970742623063345eda8df
Ifeanyi Ojo <ojoifeanyi@gmail.com>
2022-04-26 23:27:21 +0100

    another update

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './style.css';
+import food from './images/food.jpg';
 function Product() {
   return (
-    <div>
-            <h1 className='cen'>
-         Products
-            </h1>
+    <div className='wrapper'>
+             <div className='card'>
+        <div><img className='im2' src={food} alt='img'/></div>
+        <div className='card-des'>Jellof Rice</div>
+        <div><span>Order</span></div>
+        </div>
+
+        
+
+        
+
         </div>
   )
 }
